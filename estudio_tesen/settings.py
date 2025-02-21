2c6b69cc55bd8892cd7b7e63389a607ce7a254bf
Diego Alejandro Zulueta Mori <142375864+DiegoM0R1@users.noreply.github.com>
2025-02-21 11:01:37 +0000

    prueba de funcionamiento de estilos

--- estudio_tesen/settings.py
+++ estudio_tesen/settings.py
@@ -130,7 +130,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -140,3 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configuración de media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
